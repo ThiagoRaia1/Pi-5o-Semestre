cf434de0b25e8b621fd8f060f5cd4cb914a4d543
Thiago Raia de Moura <thiagoraia2004@gmail.com>
2025-04-16 02:54:35 -0300

    NavigationBar = hidden; Icone do aplicativo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,15 @@ import { Slot } from "expo-router"
 import { PaperProvider } from "react-native-paper"
 import { AuthProvider } from "../context/auth"
 import { StatusBar } from "expo-status-bar"
+import * as NavigationBar from 'expo-navigation-bar';
+import { useEffect } from "react";
 
 export default function Layout() {
+  useEffect(() => {
+    NavigationBar.setVisibilityAsync("hidden"); // Oculta a barra
+    // Você também pode mudar a cor, se quiser:
+    // NavigationBar.setBackgroundColorAsync("#ffffff");
+  }, []);
   return (
     <PaperProvider>
       <AuthProvider>
